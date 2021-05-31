c6bc9d7ca42170d6d4899e87f4a8e0b3e964fdd0
Michael Cooley <mikejames386@gmail.com>
2021-05-30 22:36:07 -0500

    load products from github json file

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	actions: {
+		async fetchProducts() {
+      const response = await axios.get('https://raw.githubusercontent.com/RepZio/TestApplication/master/test.json');
+			return response.data.items;
+		},
+	},
 })
---
+++ src/views/Products.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="products">
+    <div v-if="products">
+      <div
+        v-for="product in products"
+        :key="product.ItemID"
+      >
+        {{ product.ItemName }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Products',
+
+  data() {
+    return {
+      products: null,
+    }
+  },
+
+  created() {
+    this.loadProducts();
+  },
+
+  methods: {
+    async loadProducts() {
+      this.products = await this.$store.dispatch('fetchProducts');
+    },
+  },
+}
+</script>
